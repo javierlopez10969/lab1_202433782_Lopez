1068a9e555cdfec261fa2971d8cbd437e8c3c178
javierlopez10969 <javier.lopez@usach.cl>
2020-04-27 10:56:37 -0400

    Primer commit

---
+++ CodigoFuente/Git_202433782_LopezCampos.rkt
@@ -0,0 +1,9 @@
+;git
+(def git)
+
+;pull , recursion natural o de cola , siendo disntinto
+;al de add ; Ej (pull zonas)
+           ; ((git pull)zonas)
+
+;add , añade los cambios locales al index
+; salida : lista de nombre s de archivos 
